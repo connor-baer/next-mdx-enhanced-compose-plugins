1088d0f1544d841afdca17d0c7a945707ffd227e
Connor Bär <connor.baer@sumup.com>
2019-08-10 13:06:20 +0200

    Disable markdown plugins

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const slug = require('rehype-slug');
-const emoji = require('remark-emoji');
-const externalLinks = require('remark-external-links');
+// const slug = require('rehype-slug');
+// const emoji = require('remark-emoji');
+// const externalLinks = require('remark-external-links');
 const withPlugins = require('next-compose-plugins');
 const withMdxEnhanced = require('next-mdx-enhanced');
 
 const mdxConfig = {
-  remarkPlugins: [emoji, externalLinks],
-  rehypePlugins: [slug]
+  // remarkPlugins: [emoji, externalLinks],
+  // rehypePlugins: [slug]
 };
 
 module.exports = withPlugins([[withMdxEnhanced, mdxConfig]]);
