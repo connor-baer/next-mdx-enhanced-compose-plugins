84df0630013703167c5ae5ea97a918619767d34c
Connor Bär <connor.baer@sumup.com>
2019-08-10 14:03:36 +0200

    Document which variants work

--- next.config.js
+++ next.config.js
@@ -3,10 +3,34 @@
 // const externalLinks = require('remark-external-links');
 const withPlugins = require('next-compose-plugins');
 const withMdxEnhanced = require('next-mdx-enhanced');
+const withBundleAnalyzer = require('@next/bundle-analyzer');
 
 const mdxConfig = {
   // remarkPlugins: [emoji, externalLinks],
   // rehypePlugins: [slug]
 };
 
-module.exports = withPlugins([[withMdxEnhanced, mdxConfig]]);
+const bundleAnalyzerConfig = {
+  enabled: process.env.ANALYZE === 'true'
+};
+
+// This doesn't work.
+module.exports = withPlugins([
+  [withMdxEnhanced, mdxConfig],
+]);
+
+// This doesn't work.
+// module.exports = withPlugins([
+//   [withMdxEnhanced, mdxConfig],
+//   [withBundleAnalyzer, bundleAnalyzerConfig]
+// ]);
+
+// This works.
+// module.exports = withPlugins([
+//   withMdxEnhanced(mdxConfig),
+//   [withBundleAnalyzer, bundleAnalyzerConfig]
+// ]);
+
+
+// This works.
+// module.exports = withMdxEnhanced(mdxConfig)(withBundleAnalyzer(bundleAnalyzerConfig));
